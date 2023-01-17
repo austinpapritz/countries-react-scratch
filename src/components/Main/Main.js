443548b9546e6de825785eb786c2a6d9b34af3b0
Austin Papritz <austin@papritz.dev>
2023-01-17 15:33:21 -0800

    mapped continents as optionsbut getting uncaught (in promise) error

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,13 +3,26 @@ import './Main.css';
 
 import CountryCard from '../CountryCard/CountryCard.js';
 
-import { useCountries } from '../../hooks/useCountries.js';
+import { useCountries, useContinents } from '../../hooks/useCountries.js';
 
 export default function Main() {
+  let i = 0;
+
   const countries = useCountries();
+  const continents = useContinents();
 
   return (
     <>
+      <select className="continent-select">
+        {continents.map((continent, i) => {
+          i++;
+          return (
+            <option key={i} value={continent} className="option">
+              {continent}
+            </option>
+          );
+        })}
+      </select>
       {countries.map((country) => (
         <CountryCard key={country.id} {...country} />
       ))}
