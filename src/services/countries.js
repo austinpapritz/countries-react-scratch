bca37bcc7532d9adbd8f642016431f65bfd8b5bc
Austin Papritz <austin@papritz.dev>
2023-01-17 15:18:31 -0800

    added fetchContinents to countries.js

--- src/services/countries.js
+++ src/services/countries.js
@@ -4,3 +4,8 @@ export async function fetchCountries() {
   const resp = await client.from('countries').select('*');
   return checkError(resp);
 }
+
+export async function fetchContinents() {
+  const resp = await client.from('country_continents').select('*');
+  return checkError(resp);
+}
