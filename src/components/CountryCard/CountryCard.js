dcb627bf2efb695507ad4b46a80b523ad06a0dc4
Austin Papritz <austin@papritz.dev>
2023-01-17 15:44:57 -0800

    country flag showing on card!

--- src/components/CountryCard/CountryCard.js
+++ src/components/CountryCard/CountryCard.js
@@ -5,13 +5,7 @@ export default function CountryCard({ name, iso2 }) {
   return (
     <div className="country-card">
       <div className="details">
-        <img
-          src={`https://flagcdn.com/16x12/${iso2}.png`}
-          srcSet={`https://flagcdn.com/32x24/${iso2}.png 2x, https://flagcdn.com/48x36/${iso2}.png 3x`}
-          width="16"
-          height="12"
-          alt={`${name}`}
-        />
+        <img src={`https://flagcdn.com/96x72/${iso2.toLowerCase()}.png`} alt={`${name}`} />
         <div className="name">{name}</div>
       </div>
     </div>
