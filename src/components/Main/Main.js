9e25e9bc230e0dfb8008490572eeabcfd6ab52f8
Austin Papritz <austin@papritz.dev>
2023-01-17 15:07:04 -0800

    CountryCard component added to main and countries mapped to props

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import './Main.css';
 
+import CountryCard from '../CountryCard/CountryCard.js';
+
+import { useCountries } from '../../hooks/useCountries.js';
+
 export default function Main() {
-  return <div>Main</div>;
+  const countries = useCountries();
+
+  return (
+    <>
+      {countries.map((country) => (
+        <CountryCard key={country.id} {...country} />
+      ))}
+    </>
+  );
 }
