33bca49207e526f336cd0b4e829cb491ef14736c
Austin Papritz <austin@papritz.dev>
2023-01-17 16:26:57 -0800

    select filter works but only for the first change

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import './Select.css';
 
-export default function Select() {
-  return <div>Select</div>;
+export default function Select({ continent, setContinent }) {
+  return (
+    <select
+      className="continent-select"
+      value={continent}
+      onChange={(e) => {
+        setContinent(e.target.value);
+      }}
+    >
+      <option value="All">All</option>
+      <option value="Africa">Africa</option>
+      <option value="Antarctica">Antarctica</option>
+      <option value="Asia">Asia</option>
+      <option value="Europe">Europe</option>
+      <option value="North America">North America</option>
+      <option value="Oceania">Oceania</option>
+      <option value="South America">South America</option>
+    </select>
+  );
 }
